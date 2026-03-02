866d7c753d8735acfff0a2d1af32dc939087f64d
DiegoIpaez <diegoipaez2000@gmail.com>
2026-03-01 22:35:10 -0300

    refactor: centralize .env loading and absolute credential path in config

--- src/lib/googleSheetsClient.lib.js
+++ src/lib/googleSheetsClient.lib.js
@@ -1,24 +1,24 @@
-import { google } from "googleapis";
-import { CONFIG } from "../constants/index.js";
+import { google } from 'googleapis';
+import { CONFIG } from '../constants/index.js';
 
 const auth = new google.auth.GoogleAuth({
   keyFile: CONFIG.GOOGLE_SHEETS.KEY_JSON_PATH,
   scopes: CONFIG.GOOGLE_SHEETS.SCOPES,
 });
 
-export const googleSheetsClient = google.sheets({ version: "v4", auth });
+export const googleSheetsClient = google.sheets({ version: 'v4', auth });
 
 export async function appendSpreadsheet(
   spreadsheetId,
   range,
   values,
-  valueInputOption = "USER_ENTERED",
+  valueInputOption = 'USER_ENTERED',
 ) {
   const result = await googleSheetsClient.spreadsheets.values.append({
     spreadsheetId,
     range,
     valueInputOption,
-    insertDataOption: "INSERT_ROWS",
+    insertDataOption: 'INSERT_ROWS',
     resource: { values },
   });
 
